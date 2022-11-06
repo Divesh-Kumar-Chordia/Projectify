89829c4e52b39d99f3e6a5d4430d1ca6915c35a2
Divesh Kumar Chordia <diveshkumarchordia@gmail.com>
2022-11-06 16:50:01 +0530

    first page completed fronted part

--- share-code/src/Footer.js
+++ share-code/src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import "./Footer.css";
 function Footer() {
   return (
-    <div className='footer'>
-        <div className='footer_right'>@2022 Projectify </div>
-        <div className="footer_left"> Fork me !</div>
+    <div className="footer">
+        <div className="footer_left">@2022 Projectify </div>
+        <div className="footer_right"> 
+        <div className="footer_item">Fork me !</div>
+        </div>
     </div>
   )
 }
